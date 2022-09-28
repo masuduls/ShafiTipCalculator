51294b5bfab9590a99408cad1788cc34a1542293
Masudul Shafi <masuduls@nycstudents.net>
2022-09-28 17:29:47 -0400

    finished code + 1

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,40 @@ import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args) {
+
+        // variables
+        int totalPeople;
+        int tipPercentage;
+        double dishCost;
+        double totalCost;
+        double totalCostWithTip;
+
+        // import object initialization
         Scanner scan = new Scanner(System.in);
         DecimalFormat formatter = new DecimalFormat("#.##");
 
+        // user input
         System.out.println("Welcome to the tip calculator!");
         System.out.print("Enter the amount of people in your group: ");
-        int totalPeople = scan.nextInt();
+        totalPeople = scan.nextInt();
         scan.nextLine();
         System.out.print("Enter the tip percentage (0-100): ");
-        int tipPercentage = scan.nextInt();
+        tipPercentage = scan.nextInt();
         scan.nextLine();
-        double dishCost = 0;
-        double totalCost = 0;
+        dishCost = 0;
+        totalCost = 0;
         while (dishCost != -1) {
             System.out.print("Enter a cost in dollars and cents (-1 to end): ");
             dishCost = scan.nextDouble();
             totalCost += dishCost;
             scan.nextLine();
         }
+
+        // calculations
         totalCost ++;
         System.out.println("Total bill before tip: " + formatter.format(totalCost));
         System.out.println("Tip percentage: " + tipPercentage + "%");
-        double totalCostWithTip = totalCost * (((double) tipPercentage / 100) + 1);
+        totalCostWithTip = totalCost * (((double) tipPercentage / 100) + 1);
         System.out.println("Total tip: " + (formatter.format(totalCostWithTip - totalCost)));
         System.out.println("Total bill with tip: " + formatter.format(totalCostWithTip));
         System.out.println("Per person cost before tip: " + (formatter.format(totalCost / totalPeople)));
